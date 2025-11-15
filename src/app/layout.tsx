d35937ee76af89755fc06c5b604693acba8d74f7
Najm Hoda <najmkhoda@gmail.com>
2025-11-14 22:37:43 -0800

    Add carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>
+      <body className='p-4'>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ImageCarousel from '@/components/ImageCarousel';
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className='flex flex-col items-center'>
+      <h1>ACM Photobook</h1>
+      <ImageCarousel />
     </div>
   );
 }
